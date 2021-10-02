eb184f30c1c35a893237d9bb7e101b6fb6e0c546
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2021-10-01 22:50:30 -0600

    classUpdater

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,24 @@ var saveButton = $(".saveBtn");
 
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+var currentHour = Number(currentDay.format('H'));
+
+
+
+
+
+setInterval (function()  {
+    currentDay = moment();
+    $("#curentDay").text(currentDay.format("dddd, MMMM Do YYYY"));
+
+
+}, 1000);
+
+
+
+
+
+
+
+
