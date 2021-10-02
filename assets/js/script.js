566274ecd177539f7edd50ab71f8003759fcb4b2
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2021-10-01 23:02:17 -0600

    curent hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,56 @@ setInterval (function()  {
 
 
 
+if(currentHour === 9) {
+    $("#0").addClass("present");
+} else if(currentHour> 9) {
+    $("#0").addClass("past");
+};
+
+if(currentHour === 10) {
+    $('#1').addClass("present");
+} else if(currentHour > 10) {
+    $('#1').addClass("past");
+};
+
+if(currentHour === 11) {
+    $('#2').addClass("present");
+} else if(currentHour > 11) {
+    $('#2').addClass("past");
+};
+
+if(currentHour === 12) {
+    $('#3').addClass("present");
+} else if(currentHour > 12) {
+    $('#3').addClass("past");
+};
+
+if(currentHour === 13) {
+    $('#4').addClass("present");
+} else if(currentHour > 13) {
+    $('#4').addClass("past");
+};
+
+if(currentHour === 14) {
+    $('#5').addClass("present");
+} else if(currentHour > 14) {
+    $('#5').addClass("past");
+};
+
+if(currentHour === 15) {
+    $('#6').addClass("present");
+} else if(currentHour > 15) {
+    $('#6').addClass("past");
+};
+
+if(currentHour === 16) {
+    $('#7').addClass("present");
+} else if(currentHour > 16) {
+    $('#7').addClass("past");
+};
+
+if(currentHour === 17) {
+    $('#8').addClass("present");
+} else if(currentHour > 17) {
+    $('#8').addClass("past");
+};
